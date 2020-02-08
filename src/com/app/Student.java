ae7b29d4c0da1dac631225ce4c3cae021d4d8c63
Mazhar-PC <Mazhar-PC@DESKTOP-HGRDTRB>
2020-02-08 23:25:41 +0530

    123

--- src/com/app/Student.java
+++ src/com/app/Student.java
@@ -2,5 +2,23 @@ package com.app;
 
 public class Student {
 	private int roll;
+	private String sname;
+	public Student() {
+		
+	}
+	public int getRoll() {
+		return roll;
+	}
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	
+	
 
 }
