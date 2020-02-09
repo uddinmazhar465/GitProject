105fb7a11649b0c3570425797aed9ed098170c0d
Mazhar-PC <Mazhar-PC@DESKTOP-HGRDTRB>
2020-02-09 23:20:34 +0530

    Add Test

---
+++ src/com/app/Test.java
@@ -0,0 +1,14 @@
+package com.app;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Student st = new Student();
+		st.setRoll(1005);
+		st.setMarks(78.65);
+		System.out.println(st.getRoll());
+		System.out.println(st.getMarks());
+
+	}
+
+}
